a53cc135cf1536e893573c7b0d0b6f1f468ad55b
Elias Smith <thewillthatishiddeninside@gmail.com>
2022-01-15 00:23:29 +0200

    Now it works on the shiny hooks.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,48 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from '../components/card-list/card-list';
 import Scroll from '../components/scroll/scroll';
 import SearchBox from '../components/search-box/search-box';
 import { setSearchField, requestRobots } from '../actions';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch,} from 'react-redux';
 
-const mapToStateProps = (state) => {
-  return {
-    searchField: state.searchRobots.searchField,
-    robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
+const App = () => {
+
+  const [searchResults, setSearchResults] = useState([]);
+  const searchField = useSelector(state => state.searchRobots.searchField)
+  const robosUsers = useSelector(state => state.requestRobots.robots)
+  const isPending = useSelector(state => state.requestRobots.isPending)
+  
+  const dispatch = useDispatch();  
+  useEffect(() =>  {
+    dispatch(requestRobots());
+  }, [dispatch])
+
+  useEffect(() => {
+    let filteredRobots = robosUsers?.filter(robots => {
+      return (
+        robots.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    });
+    setSearchResults(filteredRobots);
+  }, [searchField, robosUsers])
+
+  const onSearchChange = (e) => {
+    dispatch(setSearchField(e.target.value))
+  };
+
+  return (
+    isPending ?
+      <h3 className='tc white'>Loading</h3>
+      :
+      <div className="tc">
+        <h1>Robofriends</h1>
+        <SearchBox onChange={onSearchChange} />
+        <Scroll>
+          <CardList robots={searchResults} />
+        </Scroll>
+      </div>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
-
-class App extends Component {
-
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
-  render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(r =>
-      r.name.toLowerCase().includes(searchField.toLowerCase()));
-
-    return (
-      isPending ?
-        <h3 className='tc white'>Loading</h3>
-        :
-        <div className="tc">
-          <h1>Robofriends</h1>
-          <SearchBox onChange={onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-    );
-  }
-}
-
-export default connect(mapToStateProps, mapDispatchToProps)(App);
+export default App;
